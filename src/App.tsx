6627c46df0c641149f7e1405a79d5ddeaa2678e7
oderechei <aleksandr.derechei#@gmail.com>
2021-05-04 16:05:48 +0300

    Add pages to the router

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
+import { Auth } from './pages/auth';
 import { Home } from './pages/home';
 import { NotFound } from './pages/not-found';
+import { Shop } from './pages/shop';
+import { Header } from './components/header';
 
 function App() {
   return (
-    <div>
+    <>
+      <Header />
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route exact path="/auth" component={Auth} />
+        <Route exact path="/shop" component={Shop} />
         <Route path="*" component={NotFound} />
       </Switch>
-    </div>
+    </>
   );
 }
 
