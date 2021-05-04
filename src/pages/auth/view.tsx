28e8af7ea7fa1078a50ac73f4467b66c51ae8cfc
oderechei <aleksandr.derechei#@gmail.com>
2021-05-04 16:02:52 +0300

    Add Auth page

---
+++ src/pages/auth/view.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { SignIn } from '../../components/sign-in';
+
+export const Auth = () => (
+  <div className="sign-in-and-sign-up">
+    <SignIn />
+  </div>
+);
